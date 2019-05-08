780a4f4cd47ff554f8ac7e1c41d95b5639783c9c
Mikolaj Ambroziak <mikolajambroziak2017@outlook.com>
2019-05-08 17:46:34 +0200

    lab5

--- activities/app/src/main/java/com/example/ambroziak/BornCalendary.java
+++ activities/app/src/main/java/com/example/ambroziak/BornCalendary.java
@@ -16,20 +16,21 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class BornCalendary extends AppCompatActivity {
-    private final Long dayLong=86400000L;
+    private final Long dayLong = 86400000L;
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_born_calendary);
-        Intent intent=getIntent();
-        Long  dayToBorn=intent.getLongExtra("born",0);
-        TextView tydzien=(TextView) findViewById(R.id.textView5);
-        TextView doPorodu=(TextView) findViewById(R.id.textView6);
-        CalendarView calendarView=(CalendarView) findViewById(R.id.calendarView);
+        Intent intent = getIntent();
+        Long dayToBorn = intent.getLongExtra("born", 0);
+        TextView tydzien = (TextView) findViewById(R.id.textView5);
+        TextView doPorodu = (TextView) findViewById(R.id.textView6);
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
         calendarView.setMinDate(System.currentTimeMillis());
-        tydzien.setText(String.valueOf((280-dayToBorn)/7));
+        tydzien.setText(String.valueOf((280 - dayToBorn) / 7));
         doPorodu.setText(String.valueOf(dayToBorn));
-        calendarView.setDate(System.currentTimeMillis()+dayLong*(dayToBorn));
+        calendarView.setDate(System.currentTimeMillis() + dayLong * (dayToBorn));
     }
 }
