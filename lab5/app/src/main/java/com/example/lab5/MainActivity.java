ff4e05fd851513fb472aaf2ed5fe1f8c875f5327
Mikolaj Ambroziak <mikolajambroziak2017@outlook.com>
2019-05-08 17:42:39 +0200

    lab5

---
+++ lab5/app/src/main/java/com/example/lab5/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.lab5;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements WorkoutListFragment.Listner{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       }
+
+    @Override
+    public void itemClicked(long id) {
+        Intent intent=new Intent(this,DetailActivity.class);
+        intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID,(int)id);
+        startActivity(intent);
+    }
+}
